598c840d8da1b6b5f62980faa7205fa546b51e8b
LimeFlash <daniyar@usa.com>
2019-03-12 16:32:39 +0300

    db

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 let cheerio = require("cheerio");
 let jsonframe = require("jsonframe-cheerio");
 const got = require("got");
+var fs = require('fs');
 
 async function scrapScheme() {
   const url = "https://dustyo-o.github.io/calendar";
   const html = await got(url);
   const $ = cheerio.load(html.body);
+  var counter = 0;
   jsonframe($);
   let frame = {
     title: {
-	  _s: ".wiki-head, .wiki-head__text, .wiki-doc",
+	  _s: ".wiki-head",
       _d: [
         {
 		  "name": "h2 > span",
 		  "topic": "h3 > span",
-		  "list": "ul > li"
         }
       ]
 	},
 	list: {
 		_s: ".wiki-list",
 		_d: [{
-			"list": "ul > li"
+			"list": "_parent_"
+		}]
+	},
+	date: {
+		_s:".wiki-p",
+		_d: [{
+			"date": "_parent_"
 		}]
 	}
   };
-  console.log($('body').scrape(frame, {
-	  string: true,
+  var dataDB = ($('body').scrape(frame, {
 }))
+var jsonSync = function () {
+	fs.writeFileSync('./data.json', JSON.stringify(dataDB, null, 2));
+}
+jsonSync();
 }
 scrapScheme();
